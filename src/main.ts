5ec97c9ff57d27ff2daaeb407138a2f156e67194
Pablo Aguilar <paedmaag@gmail.com>
2025-11-08 00:51:11 -0300

    Se creó el archivo principal "main.ts"
    Este archivo importa todas las funciones necesarias de entradas, la funcion para agregar tarea de forma pura e impura, y la interfaz de tareas y la funcion para crear una tarea.
    Se crea el array de tareas.
    La funcion asincrona main promete devolver un void, luego en un bucle ejecutamos el menú, de momento solo esta el case 3, para agregar tareas.
    La opcion para agregar tareas genera una constante de datos crudos que recibirá los parametros de una tarea gracias a la funcion impura, luego una vez completado esto, se genera una nueva tarea con crearTarea de data.ts, y una nueva lista siendo una copia, finalmente se actualiza la lista de tareas con la tarea agregada

---
+++ src/main.ts
@@ -0,0 +1,57 @@
+//main.ts 
+/*Es el encargado de gestionar la lista de tareas
+maneja el bucle principal y coordina las funciones puras e impuras*/
+
+import { input, close} from "./entradas";
+import { agregarTareaImpura} from "./impuras";
+import { Tarea, crearTarea } from "./data";
+import { agregarTareaPura} from "./puras";
+
+let listaDeTareas: Tarea[] = [];
+
+async function main(): Promise<void> {
+  while (true) {
+    console.log("\n==== MENÚ PRINCIPAL ====");
+    console.log("1. Ver mis tareas");
+    console.log("2. Buscar una tarea");
+    console.log("3. Agregar una tarea");
+    console.log("0. Salir");
+    console.log("==========================\n");
+    let op: string = await input("Elija una opción: ");
+
+    switch (op) {
+        case "1": {
+            
+            
+            break;
+        }
+        case "2": {
+            break;
+        }
+      case "3": {
+        const datosCrudos = await agregarTareaImpura();
+        if (datosCrudos) {
+          // Llama a la funcion pura para crear la tarea de data.ts
+          //se meten los datos crudos que nos devuelve agregarTareaImpura
+          const nuevaTarea = crearTarea(datosCrudos);
+
+          // Llama a la funcion pura que agrega tareas con spreadoperator en puras.ts
+          //se añade la lista mas la nueva tarea
+          const nuevaLista = agregarTareaPura(listaDeTareas, nuevaTarea);
+
+          // Re-asigna el estado de la lista original gracias a la copia auxiliar "nuevaLista"
+          listaDeTareas = nuevaLista;
+
+          console.log("¡Tarea agregada con éxito!");
+        }
+        await input("Presiona cualquier tecla para continuar...\n");
+        break;
+      }
+      case "0": {
+            break;
+        }
+    }
+}
+}
+
+main();
